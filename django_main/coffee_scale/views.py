b557e8f2835af86be564dcb2e70bd318d3c14b5a
Mate Stancsics <stancsics.mate@gmail.com>
2023-06-05 23:03:04 +0200

    Add dummy data generator function

--- django_main/coffee_scale/views.py
+++ django_main/coffee_scale/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def get_dummy_data(request):
+    value = 123
+
+    return HttpResponse(value)
