222aeae632d5c36afef3a0455ad1fe5943c0d6ac
Mate Stancsics <stancsics.mate@gmail.com>
2023-06-05 23:44:32 +0200

    Add separate views to render and get http response

--- django_main/coffee_scale/views.py
+++ django_main/coffee_scale/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from random import randint
+
 
 def get_dummy_data(request):
-    value = 123
+    value = randint(0, 100)
+
+    context = {"value": value}
+    return render(request, "index.html", context)
+
+
+def get_weight_dummy(request):
+    value = randint(0, 100)
 
     return HttpResponse(value)
